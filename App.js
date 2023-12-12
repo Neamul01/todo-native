3e863b29cb7b852ea98463716ae5dc2a8fc7d4d1
Neamul01 <neamulhaque2002@gmail.com>
2023-12-12 22:02:26 +0600

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import TodoScreen from "./Screens/TodoScreen";
+import DetailsScreen from "./Screens/DetailsScreen";
+import MonitorScreen from "./Screens/MonitorScreen";
+import { NavigationContainer } from "@react-navigation/native";
 
+const Stack = createStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Todo" component={TodoScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+        <Stack.Screen name="Monitor" component={MonitorScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ Screens/MonitorScreen.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const MonitorScreen = () => {
+  return (
+    <View>
+      <Text>MonitorScreen</Text>
+    </View>
+  );
+};
+
+export default MonitorScreen;
+
+const styles = StyleSheet.create({});
