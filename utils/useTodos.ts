2bb2ff6eb88ad1ee13ed990571b062975e5f2ea5
umarfchy <umarfchy@gmail.com>
2023-12-13 15:05:04 +0600

    fix useTodo

--- utils/useTodos.ts
+++ utils/useTodos.ts
@@ -1,5 +1,32 @@
-export const useTodo = async (id: number = 1) => {
+export const getTodo = async (id: number = 1) => {
   const data = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`);
   const res = await data.json();
   return res;
 };
+
+
+export const useTodo = async (id: number = 1) => {
+  const [todo, setTodo] =  useState<Todo | null>(null)
+
+  useEffect(()=>{
+      async function updateTodo(){
+        setTodo()
+      }
+
+
+
+  })
+
+  return todo
+};
+
+
+
+
+const [counter, setCouter] = useState(1)
+const todo = useTodo(counter)
+
+
+<p>
+  {todo.title}
+</p>
